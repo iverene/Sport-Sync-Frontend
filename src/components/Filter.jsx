2877f99e148abb64eb4d9e7000f0a5790cf8db24
iverene <causapiniverenegrace@gmail.com>
2025-12-01 11:20:25 +0800

    scanner in inventory is working

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -46,12 +46,12 @@ export default function Filter({
         </div>
 
         {/* Dynamic Dropdowns */}
-        {filters.map((filter, index) => (
-          <div key={index}>
+        {filters.map((filter) => (
+          <div key={filter.id}>
             <select
               value={filter.value}
               onChange={filter.onChange}
-              className="w-full px- py-2 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-navyBlue/20 focus:border-navyBlue bg-white text-sm text-slate-700 cursor-pointer"
+              className="w-full px-3 py-2 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-navyBlue/20 focus:border-navyBlue bg-white text-sm text-slate-700 cursor-pointer"
             >
               {filter.options.map((option) => (
                 <option key={option.value} value={option.value}>
